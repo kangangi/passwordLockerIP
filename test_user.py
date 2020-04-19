c7dfc85e4db6dff788c994ae59951d634f69e7e2
kangangi <wanjikukangangi@gmail.com>
2020-04-19 14:09:02 +0300

    add save user details to user class

--- test_user.py
+++ test_user.py
@@ -1,1 +1,35 @@
-import 
+import unittest #importing the unittest module
+from user import User
+
+class TestUser(unittest.TestCase):
+  '''
+  Test class that defines test cases for the User class behaviours
+  Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+  '''
+
+  def setUp(self):
+    '''
+    Set up method to run before each test cases
+    '''
+    self.new_user = User ("Diana", "24" )
+  
+
+  def test_init(self):
+    '''
+    test_init case to test whether tre object is being initialised properly
+    '''
+
+    self.assertEqual(self.new_user.login_username, "Diana")
+    self.assertEqual(self.new_user.password,"24")
+
+
+  def test_save_user(self):
+    '''
+    test_save_user test case to test if the contact object is saved into the system
+    '''
+    self.new_user.save_user() 
+    self.assertEqual(len(User.users),1)
+
+if __name__ == '__main__':
+  unittest.main()
--- user.py
+++ user.py
@@ -2,6 +2,8 @@ class User:
   '''
   Class that generates new instances of users
   '''
+  users = []
+  
   def __init__(self, login_username, password):
     '''
     ___init___ method that helps us define properties for our objects.
@@ -11,6 +13,12 @@ class User:
     '''
     self.login_username = login_username
     self.password = password
+
+  def save_user(self):
+    '''
+    save user details method saves user object into users
+    '''
+    User.users.append(self)
 
   
   
