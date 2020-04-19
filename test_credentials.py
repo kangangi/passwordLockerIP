cf432e0dbf27244fc1cdd839069b9a94c970dc53
kangangi <wanjikukangangi@gmail.com>
2020-04-19 16:36:06 +0300

    add save multiple credentials functionality and it's test

--- test_credentials.py
+++ test_credentials.py
@@ -20,12 +20,28 @@ class TestCredentials(unittest.TestCase):
     self.assertEqual(self.new_credential.account_name, "Twitter")
     self.assertEqual(self.new_credential.account_password, "100")
 
-  def test_save_credentials(self):
+  def tearDown(self):
+    '''
+    tearDown method that does clean up after each test case has run
+    '''
+    Credentials.credentials_list = []
+
+    
+  def test_save_credential(self):
     '''
     test_save_credentials test case to test if the credentials into the credentials list
     '''
     self.new_credential.save_credential()
     self.assertEqual(len(Credentials.credentials_list),1)
+
+  def test_save_multiple_credential(self):
+    '''
+    test_save_multiple_credentials to check if we can save multiple credential objects to our credentials list
+    '''
+    self.new_credential.save_credential()
+    test_credential = Credentials("Test", "500")
+    test_credential.save_credential()
+    self.assertEqual(len(Credentials.credentials_list),2)
 
 if __name__ == '__main__':
   unittest.main()
